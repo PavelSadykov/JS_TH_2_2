7a5baf27b764550a0d1fbc503acb611f3759fad0
PavelSadykov <2439747@gmail.com>
2023-12-16 23:08:17 +0500

    first commit

---
+++ script/index.js
@@ -0,0 +1,6 @@
+function marker(text, color, num) {
+    document.write(`<p style="font-family: 'Comic Sans MS';  color: ${color}; 
+     font-size: ${num}px;">${text}</p>`);
+  }
+
+  marker('Наш новый текст', 'Red', 50);
